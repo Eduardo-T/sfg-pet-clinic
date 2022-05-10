caa2400bb0b2ff567acde22ac0ba23d955134e25
Eduardo-T <laloxgames@gmail.com>
2022-05-10 14:42:00 -0500

    Added service interfaces. Closes #5.

---
+++ pet-clinic-data/src/main/java/me/eduardo/sfgpetclinic/services/PetService.java
@@ -0,0 +1,14 @@
+package me.eduardo.sfgpetclinic.services;
+
+import me.eduardo.sfgpetclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+
+    Pet save(Pet pet);
+
+    Set<Pet> findAll();
+}
---
+++ pet-clinic-data/src/main/java/me/eduardo/sfgpetclinic/services/VetService.java
@@ -0,0 +1,14 @@
+package me.eduardo.sfgpetclinic.services;
+
+import me.eduardo.sfgpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
