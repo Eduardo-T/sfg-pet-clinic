caa6455d1943744044f8a82a154d3ba21c8f6d03
Eduardo-T <laloxgames@gmail.com>
2022-06-21 11:16:20 -0500

    Added Specialties Map Impl. Closes #31.

---
+++ pet-clinic-data/src/main/java/me/eduardo/sfgpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package me.eduardo.sfgpetclinic.services;
+
+import me.eduardo.sfgpetclinic.model.Speciality;
+
+public interface SpecialtiesService extends CrudService<Speciality, Long> {
+}
---
+++ pet-clinic-data/src/main/java/me/eduardo/sfgpetclinic/services/map/SpecialityMapService.java
@@ -0,0 +1,36 @@
+package me.eduardo.sfgpetclinic.services.map;
+
+import me.eduardo.sfgpetclinic.model.Speciality;
+import me.eduardo.sfgpetclinic.services.SpecialtiesService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialityMapService extends AbstractMapService<Speciality, Long> implements SpecialtiesService {
+
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Speciality findById(Long aLong) {
+        return super.findById(aLong);
+    }
+
+    @Override
+    public Speciality save(Speciality speciality) {
+        return super.save(speciality);
+    }
+
+    @Override
+    public void delete(Speciality speciality) {
+        super.delete(speciality);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        super.deleteById(aLong);
+    }
+}
